0691ca5033e75f13a3e9ff5545e834bf03200202
ardyan.lawrence <ardyan.lawrence@maestronic.com>
2025-10-28 19:23:14 +0800

    Initial Commit

---
+++ src/test/java/org/ardyan/stepdef/HomeStepDef.java
@@ -0,0 +1,8 @@
+package org.ardyan.stepdef;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HomeStepDef {
+
+}
---
+++ src/test/java/org/ardyan/stepdef/LoginStepDef.java
@@ -0,0 +1,63 @@
+package org.ardyan.stepdef;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class LoginStepDef {
+    WebDriver driver;
+    @Before
+    public void beforeTest(){
+        driver = WebDriverManager.chromiumdriver().create();
+    }
+
+    @After
+    public void afterTest(){
+        driver.close();
+    }
+
+    @Then("user is in homepage")
+    public void userIsInHomepage() {
+        By productTitle = By.xpath("//*[@id=\"item_4_title_link\"]/div");
+        WebElement productElement = driver.findElement(productTitle);
+        assertTrue(productElement.isDisplayed());
+        assertEquals("Sauce Labs Backpack", productElement.getText());
+    }
+
+    @Given("user is on login page")
+    public void userIsOnLoginPage() {
+        driver.get("https://www.saucedemo.com/");
+    }
+
+    @And("user input username with {string}")
+    public void userInputUsernameWith(String username) {
+        By usernameInputText = By.cssSelector("input#user-name");
+        driver.findElement(usernameInputText).sendKeys(username);
+    }
+
+    @And("user input password with {string}")
+    public void userInputPasswordWith(String password) {
+        By passwordInputText = By.xpath("//*[@id=\"password\"]");
+        driver.findElement(passwordInputText).sendKeys(password);
+    }
+
+    @When("user click login button")
+    public void userClickLoginButton() {
+        By loginButton = By.id("login-button");
+        driver.findElement(loginButton).click();
+    }
+
+    @And("user see error message {string}")
+    public void userSeeErrorMessage(String error_message) {
+        assertTrue(driver.getPageSource().contains(error_message));
+    }
+}
