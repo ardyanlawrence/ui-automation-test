af2b805713cfc856c1caea582a00ad6604981cd5
ardyan.lawrence <ardyan.lawrence@maestronic.com>
2025-11-15 23:15:02 +0800

    Sauce Demo Apps

--- src/test/java/org/ardyan/BaseTest.java
+++ src/test/java/org/ardyan/BaseTest.java
@@ -4,7 +4,6 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -23,7 +22,7 @@ public class BaseTest {
 
         // Set the preferences in ChromeOptions
         options.setExperimentalOption("prefs", prefs);
-//        options.addArguments("--headless");
+        options.addArguments("--headless");
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver(options);
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
